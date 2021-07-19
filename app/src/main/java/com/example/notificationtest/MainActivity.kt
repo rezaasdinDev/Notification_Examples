b372322a26b547dfd58341f8c6c0a98cc1ca9598
Reza Asadian <rezaasadian100@gmail.com>
2021-07-19 22:42:58 +0430

    Add details and settings buttons and set actions for them

--- app/src/main/java/com/example/notificationtest/MainActivity.kt
+++ app/src/main/java/com/example/notificationtest/MainActivity.kt
@@ -31,25 +31,60 @@ class MainActivity : AppCompatActivity() {
     private fun displayNotification() {
 
         val notifivationId = 45
+
         val tapResultIntent = Intent(this, SecondActivity::class.java).apply {
 //            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+
         }
 
+
         val pendingIntent: PendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                tapResultIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
         )
 
+        //action button detail
+        val toDetailsIntent = Intent(this, DetailsActivity::class.java)
+
+
+        val toDetailPendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            toDetailsIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val toDetailAction: NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0, "Details", toDetailPendingIntent).build()
+
+        //action button settings
+        val toSettingsIntent = Intent(this, SettingsActivity::class.java)
+
+
+        val toSettingsPendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            toSettingsIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val toSettingsAction: NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0, "Settings", toSettingsPendingIntent).build()
+
+
+        //
         val notification = NotificationCompat.Builder(this@MainActivity, channelID)
-                .setContentTitle("Demo Title")
-                .setContentTitle("This is a demo notification")
-                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setAutoCancel(true)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent)
-                .build()
+            .setContentTitle("Demo Title")
+            .setContentTitle("This is a demo notification")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .addAction(toDetailAction)
+            .addAction(toSettingsAction)
+            .build()
 
         notificationManager?.notify(notifivationId, notification)
 
